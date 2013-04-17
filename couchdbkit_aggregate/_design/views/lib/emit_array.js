08dfc85a9c6f48d58429438766dfc5642ae78fad
Mike White <m@mwhite.info>
2013-04-17 14:31:15 -0400

    stash

--- couchdbkit_aggregate/_design/views/lib/emit_array.js
+++ couchdbkit_aggregate/_design/views/lib/emit_array.js
@@ -1,13 +1,29 @@
 /**
- * @param exact_keys
- * @param range_keys
- * @param data hash of data keys and number, boolean, or list<number,boolean>
- *        values for those keys
- * @param extra_keys hash of data keys and arrays of extra values to include in
- *        the key for that data key
+ * A convenience function that allows you to declaratively emit multiple fields
+ * for a single document in your map function.
+ *
+ * emit_array({
+ *      data: {'foo': 0, 'bar': 1, 'baz': 1},
+ *      key: ['a', 'b'],
+ *      extra_keys: {'baz': ['c']}
+ * });
+ *
+ * will result in the following under the hood:
+ *
+ * emit(['a', 'b', 'foo'], 0);
+ * emit(['a', 'b', 'bar'], 1);
+ * emti(['a', 'b', 'baz', 'c'], 1);
+ *
+ * @param options.data hash of data keys and number, boolean, or
+ *        list<number,boolean> values for those keys
+ * @param options.key
+ * @param options.extra_keys hash of data keys and arrays of extra values to
+ *        include in the key for that data key
  */
-function emit_array(exact_keys, range_keys, data, extra_keys) {
-    extra_keys = extra_keys || {};
+function emit_array(options) {
+    var data = options.data,
+        key = options.key,
+        extra_keys = options.extra_keys || {};
 
     for (var k in extra_keys) {
         if (typeof extra_keys[k] !== "object") {
@@ -18,10 +34,8 @@ function emit_array(exact_keys, range_keys, data, extra_keys) {
     for (var k in data) {
         if (data[k] !== null) {
             var _emit = function (val) {
-                emit(exact_keys.concat([k])
-                        .concat(range_keys)
-                        .concat(extra_keys[k] || []),
-                     val);
+                var this_key = key.concat([k]).concat(extra_keys[k] || []);
+                emit(this_key, val);
             };
 
             switch (typeof data[k]) {
@@ -34,10 +48,9 @@ function emit_array(exact_keys, range_keys, data, extra_keys) {
                 }
                 break;
             case 'number':
+            default:
                 _emit(data[k]);
                 break;
-            // anything else is unsupported for builtin _stats view and
-            // should error
             }
         }
     }
